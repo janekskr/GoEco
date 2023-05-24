cd289453dd74ae8c2083bd2c4fef94e545e17d86
Peter <piotr.lopatecki@gmail.com>
2023-05-24 20:20:01 +0200

    dodalem pierwszy formularz (prototyp), nie jest to wiele ale narazie nie mam czasu

--- src/screens/Calculator.tsx
+++ src/screens/Calculator.tsx
@@ -1,6 +1,34 @@
+import {useState} from 'react';
 const Calculator = () => {
+  const [inputValue, setInputValue] = useState()
+  const min = 0
+  const max = 100
+
+  const  handleChange = (event) => {
+		let inputValue = event.target.value
+    setInputValue(inputValue);
+
+	};
+  let ecoTest = (value) =>{
+    if(value >= 50){
+      return "Nie jesteś Eko"
+    }
+    else{
+      return "jesteś eko"
+    }
+  }
+
   return (
-    <>Calculator</>
+    <>Calculator
+      <p>Input Value: {inputValue}</p>
+      <form>
+        <p>Ile godzin twój telewizor jest używany ?( podaj godziny)</p>
+        <input type="range" min={min} max={max} value={inputValue} onChange={handleChange}></input>
+
+        <p>{ecoTest(inputValue)}</p>
+      </form>
+
+    </>
   )
 }
 

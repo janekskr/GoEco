9270108cd3638dce7b5794fa4dbf33b4e9f4ad26
Peter <piotr.lopatecki@gmail.com>
2023-05-25 20:26:17 +0200

    design kalkulatora

--- app/calculator/page.tsx
+++ app/calculator/page.tsx
@@ -23,13 +23,25 @@ const Calculator = () => {
   return (
     <>Calculator
       <p>Input Value: {inputValue}</p>
-      <form>
-        <p>Ile godzin twój telewizor jest używany ? ( podaj godziny )</p>
-        <input type="range" min={min} max={max} value={inputValue} onChange={handleChange}></input>
+      <div className='form-container'>
+        <ul className='form-nav'>
+          <li>
+            <a href="/house_CO2_prod" className='form-menu-options'>Dom</a>
+          </li>
+          <li>
+            <a href="/Transport_CO2_prod" className='form-menu-options'>Transport</a>
+          </li>
+          <li>
+            <a href="/Entertaimen_CO2_prod" className='form-menu-options'>phones, computers, itp</a>
+          </li> 
+        </ul>
+        <form>
+          <p>Ile godzin twój telewizor jest używany ? ( podaj godziny )</p>
+          <input type="range" min={min} max={max} value={inputValue} onChange={handleChange}></input>
 
-        <p>{ecoTest(+inputValue)}</p>
-      </form>
-
+          <p>{ecoTest(+inputValue)}</p>
+        </form>
+      </div>
     </>
   )
 }

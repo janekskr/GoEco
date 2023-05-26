7da89e339cbbac13f8a169166167c39bf1a0c601
Peter <piotr.lopatecki@gmail.com>
2023-05-27 01:33:44 +0200

    dodalem formluarz oraz zrobilem kalkulator na temat laptopa

--- app/calculator/entertainment/page.tsx
+++ app/calculator/entertainment/page.tsx
@@ -3,31 +3,34 @@
 import { ChangeEvent, useState } from 'react';
 
 const Calculator = () => {
-  const [inputValue, setInputValue] = useState('0')
-  const min = 0
-  const max = 100
+  const [input, setInput] = useState({
+    input1: '',
+    input2: ''
+  })
+  let min : number = 0
+  // Laptop pobiera 60W na 1 godzinę jeden 1kWh = 1000W * 1g (W * t) / 1000
+  // 1kWh = 758g CO2 
+  const energy = (time: number) => {
+    return (60 * time) / 1000
+  }
 
-  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
-    const inputValue = event.target.value
-    setInputValue(inputValue);
-  };
-
-  const ecoTest = (value: number) => {
-    if (value >= 50) {
-      return "Nie jesteś Eko"
-    }
-    return "jesteś eko"
+  const co2Emission = (consumption: number) => {
+    return (756 * energy(consumption)).toFixed(2)
+  }
+  function handleChange(event: ChangeEvent<HTMLInputElement>) {
+      const { name, value } = event.target 
+      if(name && value)setInput((prevState) => ({ ...prevState, [name]: value }));
   }
 
   return (
     <>
-      <h1 className="text-3xl font-bold self-center pb-[10px] border-b-[#29836d] border-b-[2px] w-full">Telefony, Komputery itd...</h1>
-
-      <p>Ile godzin twój telewizor jest używany ? ( podaj godziny )</p>
-      <input type="range" min={min} max={max} value={inputValue} onChange={handleChange}></input>
-
-      <p>{ecoTest(+inputValue)}</p>
-
+      <h1 className="text-3xl font-bold self-center pb-[10px] border-b-[#29836d] border-b-[2px] w-full">Transport</h1>
+      <div>
+        <label htmlFor='consumption'>Ile godziń używałeś laptopa ?</label>
+        <input min={min} value={input.input1} name="input1" type="number" className='text-[#36b796] ml-[10px] mr-[5px] p-[5px] outline-none rounded-sm' id='consumption' onChange={handleChange} />
+      </div>
+      <p>Zużyłeś: {energy(+input.input1)} Kwh</p>
+      <p>Wyprodukowałeś: {co2Emission(+input.input1)} gramów CO2</p>
     </>
   )
 }
--- app/calculator/transport/page.tsx
+++ app/calculator/transport/page.tsx
@@ -7,7 +7,7 @@ const Calculator = () => {
     input1: '',
     input2: ''
   })
-
+  let min : number = 0
   // JAK COŚ TO NA 3.785411784 litra spalonego paliwa jest  8,887 gramów CO2
 
   const liters = (consumption: number, km: number) => {
@@ -15,7 +15,7 @@ const Calculator = () => {
   }
 
   const sum = (consumption: number, km: number) => {
-    return (9 / 4) * liters(consumption, km) / 1000
+    return ((9 / 4) * liters(consumption, km)).toFixed(2)
   }
   function handleChange(event: ChangeEvent<HTMLInputElement>) {
       const { name, value } = event.target 
@@ -27,14 +27,14 @@ const Calculator = () => {
       <h1 className="text-3xl font-bold self-center pb-[10px] border-b-[#29836d] border-b-[2px] w-full">Transport</h1>
       <div>
         <label htmlFor='consumption'>Jakie jest twoje średnie spalanie na 100km ?</label>
-        <input value={input.input1} name="input1" type="number" className='text-[#36b796] ml-[10px] mr-[5px] p-[5px] outline-none rounded-sm' id='consumption' onChange={handleChange} /><span>Litrów</span>
+        <input min={min}  value={input.input1} name="input1" type="number" className='text-[#36b796] ml-[10px] mr-[5px] p-[5px] outline-none rounded-sm' id='consumption' onChange={handleChange} /><span>Litrów</span>
       </div>
       <div>
         <label htmlFor='km'>Ile kilometrów przejechałeś ?</label>
-        <input value={input.input2} name="input2" type="number" className='text-[#36b796] ml-[10px] mr-[5px] p-[5px] outline-none rounded-sm'  id='km' onChange={handleChange} /> <span> / 100km</span>
+        <input min={min} value={input.input2} name="input2" type="number" className='text-[#36b796] ml-[10px] mr-[5px] p-[5px] outline-none rounded-sm'  id='km' onChange={handleChange} /> <span> / 100km</span>
       </div>
       <p>Spaliłeś: {liters(+input.input1, +input.input2)} Litrów paliwa</p>
-      <p>Wyprodukowałeś: {sum(+input.input1, +input.input2)} kilogramów CO2</p>
+      <p>Wyprodukowałeś: {sum(+input.input1, +input.input2)} gramów CO2</p>
     </>
   )
 }

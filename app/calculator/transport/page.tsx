0c2861ca5ed1c37a62f5918a4545687492932c20
janekskr <janeczek.skrobot@gmail.com>
2023-05-26 20:54:49 +0200

    poprawa transport

--- app/calculator/transport/page.tsx
+++ app/calculator/transport/page.tsx
@@ -1,21 +1,40 @@
 'use client'
 
-import { useState } from 'react';
+import { ChangeEvent, useState } from 'react';
 
 const Calculator = () => {
-  const [input, setInput] = useState('')
+  const [input, setInput] = useState({
+    input1: '',
+    input2: ''
+  })
+
   // JAK COŚ TO NA 3.785411784 litra spalonego paliwa jest  8,887 gramów CO2
-  const sum = (liters: number) => {
-    return (9 / 4) * liters
+
+  const liters = (consumption: number, km: number) => {
+    return (consumption * km) / 100
   }
+
+  const sum = (consumption: number, km: number) => {
+    return (9 / 4) * liters(consumption, km) / 1000
+  }
+  function handleChange(event: ChangeEvent<HTMLInputElement>) {
+      const { name, value } = event.target 
+      if(name && value)setInput((prevState) => ({ ...prevState, [name]: value }));
+  }
+
   return (
     <>
       <h1 className="text-3xl font-bold self-center">Transport</h1>
-      <p>Input Value: {input}</p>
-      <p>Ile ile litrów paliwa spaliłeś ?</p>
-      <input value={input} name="age" type="number" className='text-[#36b796]' onChange={e => setInput(e.target.value)} />
-      <p>Spaliłeś: {input}</p>
-      <p>Wyprodukowałeś: {sum(+input)}gramów CO2</p>
+      <div>
+        <label htmlFor='consumption'>Jakie jest twoje średnie spalanie na 100km ?</label>
+        <input value={input.input1} name="input1" type="number" className='text-[#36b796] ml-[10px] mr-[5px] p-[5px] outline-none rounded-sm' id='consumption' onChange={handleChange} /><span>Litrów</span>
+      </div>
+      <div>
+        <label htmlFor='km'>Ile kilometrów przejechałeś ?</label>
+        <input value={input.input2} name="input2" type="number" className='text-[#36b796] ml-[10px] mr-[5px] p-[5px] outline-none rounded-sm'  id='km' onChange={handleChange} /> <span> / 100km</span>
+      </div>
+      <p>Spaliłeś: {liters(+input.input1, +input.input2)} Litrów paliwa</p>
+      <p>Wyprodukowałeś: {sum(+input.input1, +input.input2)} kilogramów CO2</p>
     </>
   )
 }

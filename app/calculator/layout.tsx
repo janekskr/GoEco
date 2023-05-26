a88e8128aff7b727b92fd715e9d0e11a6126e309
janekskr <janeczek.skrobot@gmail.com>
2023-05-26 20:07:37 +0200

    styling

--- app/calculator/layout.tsx
+++ app/calculator/layout.tsx
@@ -7,7 +7,7 @@ export default function CalculatorLayout({
     children: React.ReactNode
 }) {
     return (
-        <main className="h-[calc(100vh-80px)] flex items-center justify-between">
+        <main className="h-[calc(100vh-80px)] flex items-center justify-center">
             <div className='flex w-[90%] h-[80%] bg-[#9dedbe] rounded-[35px] shadow-2xl overflow-hidden'>
                 <Sidebar />
                 <div className="flex flex-col p-[35px] flex-1 gap-5 items-start">

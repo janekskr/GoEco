5eef5a8ea3b767cd23919f30307a8c5d17f0924f
Jan Skrobot <114176438+janekskr@users.noreply.github.com>
2023-06-05 19:59:41 +0200

    Update page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,7 +2,23 @@
 
 const About = () => {
   return (
-    <div>page</div>
+    <div>
+<p>
+GoEco is a project made for competition [Mini Hackaton](https://nowaakademia.org/mini-hackathon/mini-hackathon-edycja-iii/). Its main purpose is to show the world how bad are climate changes. GoEco allows you to check your carbon footprint. You can see our source code on <a href="https://github.com/janekskr/GoEco" target="_blank">GitHub</a>
+</p>
+<p>In this project we used vast amount of modern technologies such as:</p>
+
+<ul>
+<li><a>Next.js</a> and <a>React.js</a></li>
+<li><a>Typescript</a></li>
+<li><a>Tailwind CSS</a></li>
+<li><a>React Three Fiber</a> and <a>Three.js</a></li>
+<li><a>Postcss</a></li>
+
+</ul>
+
+</div>
+
   )
 }
 

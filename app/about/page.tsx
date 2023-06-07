e9156a361f9d081fc08837152f09966e36e6d4f9
janekskr <janeczek.skrobot@gmail.com>
2023-06-07 20:36:40 +0200

    poprawiłem ikonki SVG bo coś nie działo

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-import CustomSvg from "@/components/CustomSvg"
 
 const About = () => {
   return (
@@ -14,10 +13,15 @@ const About = () => {
       <p className="leading-8 text-center sm:text-left">
         GoEco to projekt zrobiony na konkurs <a href="https://nowaakademia.org/mini-hackathon/mini-hackathon-edycja-iii/" target="_blank" className="text-white font-bold relative">
           Mini Hackaton
-          <CustomSvg StrokeWidth="16" />
+          <svg className="w-full left-0 absolute stroke-[#02674e] top-[4px]" width="244" height="40" viewBox="0 0 244 40" fill="none">
+            <path opacity="0.5" d="M3 21C20.1231 17.8525 37.9303 16.8192 55.3857 16.0719C84.6481 14.819 114.132 14.5194 143.388 15.9599C169.598 17.2503 195.455 20.3554 221.642 17.7519C228.347 17.0853 237.394 16.9679 244 16.9679" strokeWidth="16" strokeLinejoin="round" />
+          </svg>
           </a>. Jego głównym celem jest pokazanie społeczeństwu jak złe dla środowiska są zmiany klimatyczne. GoEco pozawla na zmierzenie swojego śladu węglowego. Możesz zobaczyć nasz kod źródłowy na <a href="https://github.com/janekskr/GoEco" target="_blank" className="text-white font-bold relative">
           Githubie
-          <CustomSvg Top="4px" />
+          {/* <CustomSvg Top="4px" /> */}
+            <svg className={`w-full left-0 absolute stroke-[#02674e] top-[4px]`} width="244" height="40" viewBox="0 0 244 40" fill="none">
+            <path opacity="0.5" d="M3 21C20.1231 17.8525 37.9303 16.8192 55.3857 16.0719C84.6481 14.819 114.132 14.5194 143.388 15.9599C169.598 17.2503 195.455 20.3554 221.642 17.7519C228.347 17.0853 237.394 16.9679 244 16.9679" strokeWidth="30" strokeLinejoin="round" />
+          </svg>
           </a>.
       </p>
       <p className="my-5 text-center sm:text-left">W tym projekcie użyliśmy wiele nowoczesnych narzędzi takich jak:</p>

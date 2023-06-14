3eaf8efa9b62956878cc7c768b2b905f5db1f6f3
Jan Skrobot <114176438+janekskr@users.noreply.github.com>
2023-06-14 14:23:52 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,8 +20,8 @@ export default function RootLayout({
 }) {  
   return (
     <html lang="en">
-      <body className={`${font.className} overflow-x-hidden w-screen`}>
-        <div className="min-h-screen w-full bg-[url('/img/background.svg')] bg-no-repeat bg-cover text-white">
+      <body className={font.className>
+        <div className="min-h-screen w-screen overflow-x-hidden bg-[url('/img/background.svg')] bg-no-repeat bg-cover text-white">
           <Navbar />
           {children}
           <Footer />

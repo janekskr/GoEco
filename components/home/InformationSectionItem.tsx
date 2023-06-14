a9830f5351311121b676f4c876bc758097067c0b
Jan Skrobot <114176438+janekskr@users.noreply.github.com>
2023-06-14 14:03:07 +0200

    Update InformationSectionItem.tsx

--- components/home/InformationSectionItem.tsx
+++ components/home/InformationSectionItem.tsx
@@ -16,7 +16,7 @@ const InformationSectionItem = ({title, content}: {title: string; content: strin
                     content.slice(0, 300)
                 }
               </p>
-              <p className="text-center text-lg mt-5 hiddcen:block">
+              <p className="text-center text-lg mt-5 hidden sm:block">
                 {content}
               </p>
               <button className=" mx-auto mt-[25px] block sm:hidden px-[20px] py-[10px] shadow-md hocer:shadow-2xl bg-green-800 text-lg font-bold rounded-[20px] text-white" onClick={() => setShowMore(prev => !prev)}>

c2a09d0215f1ad5809c13128612af896a835fd07
janekskr <janeczek.skrobot@gmail.com>
2023-06-14 13:21:43 +0200

    popraweczka

---
+++ components/home/InformationSection.tsx
@@ -0,0 +1,23 @@
+import { Slide } from "react-awesome-reveal"
+import InformationSectionItem from "./InformationSectionItem"
+
+const InformationSection = () => {
+  return (
+    <div className="flex flex-col gap-10 items-center w-[80%] md:w-[65%] mx-auto mt-7"> 
+          <Slide className="overflow-hidden rounded-[40px] shadow-2xl" delay={100}>
+            <InformationSectionItem 
+                title="Czemu powinno cię to obchodzić zanieczyszczenie powietrza?"
+                content="Wiele osób cierpi z powodu zanieczyszczenia powietrza, nie zdając sobie z tego sprawy. Ty możesz być jedną z nich! Duża część ludzkości nie rozumie powagi tej sytuacji. 7 milionów - taka jest liczba osób, które zmarły PRZEDWCZEŚNIE z powodu zanieczyszczenia powietrza i jego skutków. Jest również bardzo możliwe, że kontaminacja powietrza przyczyniła się do śmierci wielu starszych osób. Objawy wywoływane przez zanieczyszczenie powietrza są często bagatelizowane. To błąd, ponieważ w dłużeszej perspektywie czasu mogą prowadzić do ciękich chorób takich jak: rak płuc, udar, choroby serca, ostre i przewlekłe choroby układu oddechowego. Z tego powodu tak ważne jest więc dbanie o to zby nasze powietrze było czyste. Dbając o czyste powietrze, dbamy o swoje zdrowię"
+            />
+          </Slide>
+          <Slide className="overflow-hidden rounded-[40px] shadow-2xl" direction="right" delay={100}>
+            <InformationSectionItem
+                title="Co możesz zrobić, żeby zwalczyć zanieczyszczenie powietrza?"
+                content="Jeżeli chodzi o zwalczanie zanieczyszczenia powietrza, to najlepszą rzeczą jaką możesz zrobić to poinformować innych o problemie, sprzeciwiać się wycince roślin usuwających CO2 i wspierać fundacje, które sadzą więcej roślinności. Mimo, że zwalczanie obecnego zanieczyszczenia jest bardzo potrzebne jest to jedynie pół problemu - trzeba również zwalczyć źródła kontaminacji powietrza. Ważne jest pamiętać, że choć pojedynczy człowiek może niewiele, to grupy osób mogą zdziałać dużo więcej. Do rzeczy, które możesz zrobić należą: zmiana formy transportu - najlepiej zastąpić jazdę autem na rzecz pociągu, autobusu, roweru lub zwykłego spaceru (jeśli jest to niemożliwe to zmiana auta na hybrydowe lub elektryczne jest krokiem w dobrą stronę), możesz także rozważyć ogrzewanie domowe na elektryczne lub gazowe."
+            />
+          </Slide>
+      </div>
+  )
+}
+
+export default InformationSection
---
+++ components/home/InformationSectionItem.tsx
@@ -0,0 +1,26 @@
+"use client"
+
+import { useState } from "react"
+
+const InformationSectionItem = ({title, content}: {title: string; content: string}) => {
+    const [showMore, setShowMore] = useState<boolean>(false)
+  return (
+    <div className="p-[40px] bg-[#1f6b3c]">
+              <h1 className=" text-xl md:text-3xl sm:text-2xl font-bold text-center">
+                {title}
+              </h1>
+              <p className="text-center md:text-lg mt-5">
+                {
+                    showMore ? 
+                    content :
+                    content.slice(0, 200)
+                }
+              </p>
+              <button className=" mx-auto mt-[25px] block sm:hidden px-[20px] py-[10px] shadow-md hocer:shadow-2xl bg-green-800 text-lg font-bold rounded-[20px] text-white" onClick={() => setShowMore(prev => !prev)}>
+                Pokaż {showMore ? " mniej" : "więcej"}
+              </button>
+            </div>
+  )
+}
+
+export default InformationSectionItem
